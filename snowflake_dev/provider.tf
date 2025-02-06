e223a67a956b1ce362031f13257a0b42e4e56781
Venkata Sai Emmidesetty <141021513+Sai-venkata-emmidesetty@users.noreply.github.com>
2025-02-06 17:33:27 +0530

    Update provider.tf

--- snowflake_dev/provider.tf
+++ snowflake_dev/provider.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "snowflake" {
-  account   = var.snowflake_account
+  snowflake_account   = var.snowflake_account
   user  = var.snowflake_user
   password  = var.snowflake_password
   role      = var.snowflake_role
